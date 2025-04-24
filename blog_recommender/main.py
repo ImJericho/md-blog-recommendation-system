cba74130a9927a099a11128e3e8d4fe9894aa49e
Vivek Patidar <77988917+ImJericho@users.noreply.github.com>
2025-04-24 16:12:57 +0530

    my_version_v0.1

--- blog_recommender/main.py
+++ blog_recommender/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .api.recommendation import router as recommendation_router
-from .models.database import Base, engine
+# from .models.database import Base, engine
 
 # Create database tables
 # Base.metadata.create_all(bind=engine)
