ed0c0ef44c3c314896b0ee3f7368b81077001330
Vivek Patidar <77988917+ImJericho@users.noreply.github.com>
2025-04-24 16:13:18 +0530

    my_version_v0.2

--- blog_recommender/main.py
+++ blog_recommender/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .api.recommendation import router as recommendation_router
-# from .models.database import Base, engine
+from .models.database import Base, engine
 
 # Create database tables
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Blog Recommendation System",
